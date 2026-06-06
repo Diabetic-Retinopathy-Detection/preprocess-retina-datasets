d3b9c1ab0a143b8ca49976d418a6a918c7a25fac
Meier-Stefan <42180739+Meier-Stefan@users.noreply.github.com>
2026-06-06 20:14:08 +0100

    feat: export build_dataset_index from package __init__

--- src/preprocess_retina_datasets/__init__.py
+++ src/preprocess_retina_datasets/__init__.py
@@ -5,11 +5,13 @@ from .crop import (
     square_center_bbox,
 )
 from .errors import ImageProcessingError, PreprocessError
+from .package import build_dataset_index
 from .saliency import generate_saliency_dataset, generate_saliency_map
 
 __all__ = [
     "ImageProcessingError",
     "PreprocessError",
+    "build_dataset_index",
     "crop_dataset",
     "crop_image",
     "detect_retina_bbox",
